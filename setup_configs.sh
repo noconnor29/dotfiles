6abcbe99f233a56e76c7b2f4cc956f1fcb9e49cd
Nick O'Connor <36704600+noconnor29@users.noreply.github.com>
2022-03-21 23:25:06 -0400

    added bash aliases updated gitignore and setup script

--- setup_configs.sh
+++ setup_configs.sh
@@ -10,8 +10,9 @@ ln -sf ~/.userconfigs/.vimrc ~/.vimrc
 #Link ~/.profile
 ln -sf ~/.userconfigs/.profile ~/.profile
 
-# Link ~/.bashrc
+# Link ~/.bashrc and ~/.bash_aliases
 ln -sf ~/.userconfigs/.bashrc ~/.bashrc
+ln -sf ~/.userconfigs/.bash_aliases ~/.bash_aliases
 
 # Will also need to run manually for veeam
 # sudo ln -s /home/nick/.run_veeam_backup.sh /etc/profile.d/run_veeam_backup.sh
