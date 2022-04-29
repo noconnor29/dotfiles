1c5e29279b182ffe8459074a812baed947f09c5b
Nick O'Connor <36704600+noconnor29@users.noreply.github.com>
2022-04-29 13:48:01 -0400

    Update run_veeam_backup.sh

--- run_veeam_backup.sh
+++ run_veeam_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # requires .deb install of veeam agent
-veeamconfig job start --name "x1"
+veeamconfig job start --name "x1-linux"
 
 # link to /etc/profile.d
 # sudo ln -s /home/nick/.userconfigs/run_veeam_backup.sh /etc/profile.d/run_veeam_backup.sh
