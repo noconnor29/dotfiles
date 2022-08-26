67efe1618820afe6575307add88605d55854959f
Nick O'Connor <36704600+noconnor29@users.noreply.github.com>
2022-08-25 20:50:36 -0400

    Update setup_configs.sh

--- setup_configs.sh
+++ setup_configs.sh
@@ -10,7 +10,6 @@ ln -sf ~/.userconfigs/.vimrc ~/.vimrc
 # Link ~/.bashrc and ~/.bash_aliases
 ln -sf ~/.userconfigs/.bashrc ~/.bashrc
 ln -sf ~/.userconfigs/.bash_aliases ~/.bash_aliases
-ln -sf ~/.userconfigs/.bash_profile ~/.bash_profile
 
 # Will also need to run manually for veeam
 # sudo ln -s /home/nick/.run_veeam_backup.sh /etc/profile.d/run_veeam_backup.sh
