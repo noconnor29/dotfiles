917d2f5234364dd22b7cc9cf98e855752249bacb
Nick OConnor <36704600+noconnor29@users.noreply.github.com>
2023-03-13 17:22:03 -0400

    updated paths

--- setup_configs.sh
+++ setup_configs.sh
@@ -2,17 +2,17 @@
 # Used to move/link configs into user locations
 
 # Link ~/.ssh/config
-ln -sf ~/projects/.userconfigs/.ssh/config ~/.ssh/config
+ln -sf ~/projects/dotfiles/.ssh/config ~/.ssh/config
 
 # Link ~/.gitconfig
-ln -sf ~/projects/.userconfigs/.gitconfig ~/.gitconfig
+ln -sf ~/projects/dotfiles/.gitconfig ~/.gitconfig
 
 # Link ~/.vimrc
-ln -sf ~/projects/.userconfigs/.vimrc ~/.vimrc
+ln -sf ~/projects/dotfiles/.vimrc ~/.vimrc
 
 # Link ~/.bashrc and ~/.bash_aliases
-ln -sf ~/projects/.userconfigs/.bashrc ~/.bashrc
-ln -sf ~/projects/.userconfigs/.bash_aliases ~/.bash_aliases
+ln -sf ~/projects/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/projects/dotfiles/.bash_aliases ~/.bash_aliases
 
 # Will also need to run manually for veeam
 # sudo ln -s /home/nick/.run_veeam_backup.sh /etc/profile.d/run_veeam_backup.sh
